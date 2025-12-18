81abd09614a51b69795f93da0a3c356291b69bd7
Ushasri17 <usri71168@gmail.com>
2025-12-18 18:00:48 +0530

    changing app.jsx

--- 3_Fragments/Fragments/src/App.jsx
+++ 3_Fragments/Fragments/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <React.Fragment>
+    <>
       <h1>List:</h1>
       <ul class="list-group">
         <li class="list-group-item">An item</li>
@@ -12,7 +12,7 @@ function App() {
         <li class="list-group-item">A fourth item</li>
         <li class="list-group-item">And a fifth one</li>
       </ul>
-    </React.Fragment>
+    </>
   );
 }
 
